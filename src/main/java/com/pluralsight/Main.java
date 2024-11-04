42ba5194e780a4e288a81fb3662ed450b767c313
Nikesh Sitaula <sitaulanike@gmail.com>
2024-11-04 10:48:07 -0500

    on file manager

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,6 +3,6 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        new UserInterface();
     }
 }
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -90,7 +90,4 @@ public class UserInterface {
             System.out.println(vehicle);
         }
     }
-
-
-
 }
